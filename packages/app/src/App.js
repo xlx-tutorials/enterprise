6d42180409015d13ad6fc9c7d39a7bae12dbb0b5
iamyoki <yokiyuqian@gmail.com>
2021-07-27 21:58:53 +0800

    feat: add json-server

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,9 +1,15 @@
 import GlobalCSS from 'components/GlobalCSS'
 import Providers from 'containers/Providers'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Routes from 'Routes'
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, [])
+
   return (
     <Providers>
       <GlobalCSS />
