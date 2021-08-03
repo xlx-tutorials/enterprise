66dfafe453a84251aa0d5b165fcbd9c23ab942af
iamyoki <yokiyuqian@gmail.com>
2021-08-03 20:17:08 +0800

    feat: add handleTheme

---
+++ packages/app/public/handleTheme.js
@@ -0,0 +1,11 @@
+// const mode = localStorage.getItem('themeMode')
+
+// window.addEventListener('storage', (ev) => {
+//   console.log(ev)
+// })
+
+// if (mode === `"dark"`) {
+//   document.body.style.backgroundColor = '#0f0f10'
+// } else {
+//   document.body.style.backgroundColor = '#ffffff'
+// }
