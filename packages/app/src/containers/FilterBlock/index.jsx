320911b653ae06ffc8d318936985cedc4befc55d
iamyoki <yokiyuqian@gmail.com>
2021-08-06 20:34:58 +0800

    feat: fetch filter data

--- packages/app/src/containers/FilterBlock/index.jsx
+++ packages/app/src/containers/FilterBlock/index.jsx
@@ -1,104 +1,50 @@
 import { Block } from 'components/Block'
 import Radio from 'components/Radio'
-import { useRadioGroup } from 'components/Radio/hooks'
+import { useRadio } from 'components/Radio/hooks'
 import { Button } from 'containers/DemoPage/components/Button'
+import { useEffect, useState } from 'react'
 import { FilterLabel, FilterRow } from './components/styled'
 
-const fakeData = [
-  {
-    label: '快速筛选',
-    radio: [
-      {
-        label: '筛选项名1',
-        value: '筛选项名1',
-      },
-      {
-        label: '筛选项名2',
-        value: '筛选项名2',
-      },
-      {
-        label: '筛选项名3',
-        value: '筛选项名3',
-      },
-      {
-        label: '筛选项名4',
-        value: '筛选项名4',
-      },
-      {
-        label: '筛选项名5',
-        value: '筛选项名5',
-      },
-    ],
-  },
-  {
-    label: '街道地区',
-    radio: [
-      {
-        label: '街道1',
-        value: '街道1',
-      },
-      {
-        label: '街道2',
-        value: '街道2',
-      },
-      {
-        label: '街道3',
-        value: '街道3',
-      },
-      {
-        label: '街道4',
-        value: '街道4',
-      },
-      {
-        label: '街道5',
-        value: '街道5',
-      },
-    ],
-  },
-  {
-    label: '行业分类',
-    radio: [
-      {
-        label: '行业分类1',
-        value: '行业分类1',
-      },
-      {
-        label: '行业分类2',
-        value: '行业分类2',
-      },
-      {
-        label: '行业分类3',
-        value: '行业分类3',
-      },
-      {
-        label: '行业分类4',
-        value: '行业分类4',
-      },
-      {
-        label: '行业分类5',
-        value: '行业分类5',
-      },
-    ],
-  },
-]
-
 function FilterBlock() {
-  const { bind, groupValue, clear, recoverLastSelection } = useRadioGroup()
+  const { bind: bindStreetsRadio, clear: clearStreetsRadio } = useRadio()
+  const [streets, setStreets] = useState([])
+  const { bind: bindIndustiesRadio, clear: clearIndustriesRadio } = useRadio()
+  const [industies, setIndusties] = useState([])
+
+  useEffect(function fetchData() {
+    fetch('http://localhost:5000/streets')
+      .then((res) => res.json())
+      .then(setStreets)
+
+    fetch('http://localhost:5000/industries')
+      .then((res) => res.json())
+      .then(setIndusties)
+  }, [])
 
   return (
     <Block className='Filter'>
-      {fakeData.map((item, i) => (
-        <FilterRow key={item.label}>
-          <FilterLabel>{item.label}</FilterLabel>
-          <Radio {...bind(i)}>
-            {item.radio.map((option) => (
-              <Radio.Option key={option.value} value={option.value} noCircle>
-                {option.label}
-              </Radio.Option>
-            ))}
-          </Radio>
-        </FilterRow>
-      ))}
+      <FilterRow>
+        <FilterLabel>街道地区</FilterLabel>
+        <Radio {...bindStreetsRadio()}>
+          {streets.map((option) => (
+            <Radio.Option key={option.id} value={option.name} noCircle>
+              {option.name}
+            </Radio.Option>
+          ))}
+        </Radio>
+      </FilterRow>
+
+      <FilterRow>
+        <FilterLabel>行业分类</FilterLabel>
+        <Radio {...bindIndustiesRadio()}>
+          {industies.map((option) => (
+            <Radio.Option key={option.id} value={option.name} noCircle>
+              {option.name}
+            </Radio.Option>
+          ))}
+        </Radio>
+      </FilterRow>
+
       <FilterRow
         style={{
           padding: '12px 18px',
@@ -106,17 +52,12 @@ function FilterBlock() {
         <Button
           style={{
             marginLeft: 'auto',
-          }}
-          // eslint-disable-next-line no-unneeded-ternary
-          onClick={recoverLastSelection ? recoverLastSelection : () => {}}>
-          {recoverLastSelection ? '恢复' : ''}
-        </Button>
-        <Button
-          style={{
-            marginLeft: 20,
             color: 'orangered',
           }}
-          onClick={clear}>
+          onClick={() => {
+            clearStreetsRadio()
+            clearIndustriesRadio()
+          }}>
           重置
         </Button>
       </FilterRow>
