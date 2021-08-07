7abe5c003a6986e67c170506f21f51db300175f3
iamyoki <yokiyuqian@gmail.com>
2021-08-07 20:50:32 +0800

    feat: add search

--- packages/app/src/containers/ListBlock/index.jsx
+++ packages/app/src/containers/ListBlock/index.jsx
@@ -1,18 +1,22 @@
 import { Link } from '@reach/router'
 import { Block } from 'components/Block'
 import React from 'react'
+import { useQueryParam } from 'use-query-params'
 import ListItem from './components/ListItem'
 import { SortButton, SortContainer, SortLabel } from './components/styled'
 import { useFetchListData } from './hooks/useFetchListData'
 
 function ListBlock() {
   const { data } = useFetchListData()
+  const [keyword] = useQueryParam('keyword')
+
+  const filteredData = data.filter((item) => item.companyName.includes(keyword))
 
   return (
     <Block className='ListBlock'>
       <SortContainer>
         <SortLabel>
-          共找到<strong>{data?.length}</strong>条结果
+          共找到<strong>{filteredData?.length}</strong>条结果
         </SortLabel>
 
         <SortButton>营业收入</SortButton>
@@ -23,7 +27,7 @@ function ListBlock() {
         <SortButton>营业收入</SortButton>
       </SortContainer>
 
-      {data?.map((item, i) => (
+      {filteredData?.map((item, i) => (
         <ListItem key={i.toString()} as={Link} to={`/${item.id}`} data={item} />
       ))}
     </Block>
